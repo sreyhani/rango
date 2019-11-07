053d792e9cb9190ab362223e1ad2c4d0fc825341
mahdi5235 <mahdi.haji@yahoo.com>
2019-11-07 19:30:38 +0330

    email - secure - complete

--- rango/settings.py
+++ rango/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sajjad7997@gmail.com'
-EMAIL_HOST_PASSWORD = 'deltorabook'
+EMAIL_HOST_USER = 'testforwebelopers19@gmail.com'
+EMAIL_HOST_PASSWORD = 'mest7510bew'
