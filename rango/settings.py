9c38b6fdc564d7b9028b81bbb6b008206bf5f929
mahdi5235 <mahdi.haji@yahoo.com>
2019-11-07 22:23:15 +0330

    email - secure - completed

--- rango/settings.py
+++ rango/settings.py
@@ -115,11 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = 'deltorabook'
-# EMAIL_PORT = 587
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
