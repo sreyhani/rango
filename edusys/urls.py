8a236f32c8afd557ed9ae4641104505b40c046cb
mahdi5235 <mahdi.haji@yahoo.com>
2019-11-07 16:29:19 +0330

    login

--- edusys/urls.py
+++ edusys/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from edusys import views
-from edusys.views import home
+from edusys.views import home, login_view, logout_view
 
 urlpatterns = [
     path('', home),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+
 ]
--- edusys/views.py
+++ edusys/views.py
@@ -1,10 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.models import User
+from django.http import HttpResponse, request
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def home(req):
     return render(req, "homepage.html")
 
-def signup(req):
-    pass
+
+def login_view(req):
+    if req.POST:
+        print(req)
+        username = req.POST['username']
+        password = req.POST['password']
+        user = authenticate(req, username=username, password=password)
+        print(req.user.is_authenticated)
+        if user:
+            login(req, user)
+            return redirect('/')
+        else:
+            return render(req, 'login.html', {"error": False})
+    return render(req, 'login.html', {"error": True})
+
+
+def logout_view(req):
+    logout(req)
+    return redirect('/')
