9dafe774b28b11b1cdd7941c5e1cb1ab16ad8b42
mahdi5235 <mahdi.haji@yahoo.com>
2019-11-07 22:53:57 +0330

    Course

--- edusys/models.py
+++ edusys/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    department = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    course_number = models.IntegerField()
+    group_number = models.IntegerField()
+    teacher = models.CharField(max_length=30)
+    time_choices = ['0', '1', '2', '3', '4']
+    start_time = models.DateTimeField()
+    end_time = models.IntegerField(max_length=30)
+    first_day = models.CharField(max_length=1, choices=time_choices)
+    end_day = models.IntegerField(max_length=30)
