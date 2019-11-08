65e3c521cd8aa55330061ea251800f7435301fa8
sreyhani <sajjad7997@gmail.com>
2019-11-08 09:14:08 +0330

    fixed

--- edusys/models.py
+++ edusys/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     course_number = models.IntegerField()
     group_number = models.IntegerField()
     teacher = models.CharField(max_length=30)
-    time_choices = ['0', '1', '2', '3', '4']
+    time_choices = [('0','شنبه'), ('1','یکشنبه'), ('2','دوشنبه'), ('3','سه‌شنبه'), ('4','چهارشنبه')]
     start_time = models.DateTimeField()
     end_time = models.IntegerField(max_length=30)
     first_day = models.CharField(max_length=1, choices=time_choices)
