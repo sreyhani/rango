9acce8f97b9927f33ca6135a35b3d1a14745a73c
sreyhani <sajjad7997@gmail.com>
2019-11-08 15:16:20 +0330

    ?

--- edusys/forms.py
+++ edusys/forms.py
@@ -23,7 +23,6 @@ class ContactUs(forms.Form):
 
 class CourseForm(forms.ModelForm):
     # exam_date = forms.DateField(input_formats='%Y-%m-%d')
-
     class Meta:
         model = Course
         fields = "__all__"
